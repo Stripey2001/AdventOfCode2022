c7a8f6114891500edf5815677016b35cf8dbc023
Stripey2001 <53921537+Stripey2001@users.noreply.github.com>
2022-12-05 09:00:07 +0000

    Update Day5Part1.py

--- Day5Part1.py
+++ Day5Part1.py
@@ -1,17 +1,32 @@
 Input = []
 LineReader = open("basic.txt","r")
 for line in LineReader:
-    Input.append(line)
+    Input.append(line[:-1])
+
 Crates=[]
-for i in range(0,int(len(Input[0])/4)):
-    Crates.append([])
-Image=True
-i=0
-while Image:
-    if Input[i][1] == "1":
-        Image = False
+for i in range(0,int((len(Input[0])+1)/4)):
+    Crates.append([])    
+
+for i in range(0,len(Input)):
+    if Input[i] == "":
         break
-    for j in range(0,int(len(Input[0])/4)):
-        Crates[j].append(Input[i][(j*4)+1]) 
-    i+=1
+Seperate = i
+i+=-1
+while i > 0:
+    i+=-1
+    for j in range(0,int((len(Input[0])+1)/4)):
+        if Input[i][(j*4)+1] != " ":
+            Crates[j].append(Input[i][(j*4)+1]) 
+    
 print(Crates)
+
+def MoveCrates(Amount, Target, Destination):
+    for i in range(-Amount,0,-1):
+        Target.append(Destination.pop(i))
+    return Target, Destination
+
+for i in range(Seperate+1,len(Input)):
+    Line = Input[i].split(" ")
+    Crates[int(Line[3])], Crates[int(Line[5])] = MoveCrates(int(Line[1]),Crates[int(Line[3])], Crates[int(Line[5])])
+    print(Crates)
+    #print(Line)
